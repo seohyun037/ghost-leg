d8aada3ec73c1c77f417cdab6381a7a9ac9f1a03
seohyun037 <bonopark08@gmail.com>
2024-10-23 23:06:33 +0900

    added some variables

--- game.py
+++ game.py
@@ -2,27 +2,42 @@ from matplotlib import pyplot as plt
 import random
 
 start = int(input("어디서 시작할지 숫자로 입력: "))
-a = [1,2,3,4,5,6,7]
-b = [0,0,0,0,0,0,0]
 
-for _ in range(1000): # 1000번 반복
+"""people: 사람 인원수
+bridge: 가로 사다리 수
+rail = 2bridge: 세로 사다리 수"""
+
+people = 7
+bridge = 15 # 가로
+rail = 20 # 세로
+
+a = []
+b = []
+line = []
+
+for i in range(people):
+    a.append(i+1)
+    b.append(0)
+    line.append('│')
+
+for _ in range(10000): # 10000번 반복
     ladder = 0
     player = start - 1
     board = []
-    for i in range(20):
-        board.append(['│', '│', '│', '│', '│', '│', '│']) # 7명, 20개의 세로 사다리
+    for i in range(rail):
+        board.append(line.copy())
 
 
-    while ladder < 15: # 가로 사다리 15개 배치
-        x = random.randint(0, 19)
-        y = random.randint(0,5)
+    while ladder < bridge: 
+        x = random.randint(0, rail - 1)
+        y = random.randint(0, people - 2)
         if board[x][y] == '─' or board[x][y + 1] == '─':
             continue
         board[x][y] = '─'
         board[x][y + 1] = '─'
         ladder += 1
 
-    for i in range(20):
+    for i in range(rail):
         cnt = 0
         if board[i][player] == '─':
             for j in range(player):
